521e045c1fbba0979985503e92ad38c9cc65a195
Bridgett Zaouga <BZaouga@se.linedata.com>
2026-05-10 20:29:57 +0100

    workshop3: déplacement ListSuggestion dans module suggestions (lazy loading)

--- src/app/features/suggestions/suggestions-routing.module.ts
+++ src/app/features/suggestions/suggestions-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SuggestionsComponent } from './suggestions.component';
+import { ListSuggestionComponent } from './list-suggestion/list-suggestion.component';
 
-const routes: Routes = [{ path: '', component: SuggestionsComponent }];
+const routes: Routes = [{ path: '', component: ListSuggestionComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/suggestions/suggestions.module.ts
+++ src/app/features/suggestions/suggestions.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { SuggestionsRoutingModule } from './suggestions-routing.module';
 import { SuggestionsComponent } from './suggestions.component';
-
+import { ListSuggestionComponent } from './list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
-    SuggestionsComponent
+    SuggestionsComponent,
+    ListSuggestionComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SuggestionsRoutingModule
   ]
 })
