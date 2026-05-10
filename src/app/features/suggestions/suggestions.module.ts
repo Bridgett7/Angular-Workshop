dc154dab9d2a5bfea6136526945a72597959bef6
Bridgett Zaouga <BZaouga@se.linedata.com>
2026-05-10 20:46:22 +0100

    workshop4: création composant SuggestionForm

--- src/app/features/suggestions/suggestions.module.ts
+++ src/app/features/suggestions/suggestions.module.ts
@@ -6,12 +6,14 @@ import { SuggestionsRoutingModule } from './suggestions-routing.module';
 import { SuggestionsComponent } from './suggestions.component';
 import { ListSuggestionComponent } from './list-suggestion/list-suggestion.component';
 import { SuggestionDetailsComponent } from './suggestion-details/suggestion-details.component';
+import { SuggestionFormComponent } from './suggestion-form/suggestion-form.component';
 
 @NgModule({
   declarations: [
     SuggestionsComponent,
     ListSuggestionComponent,
-    SuggestionDetailsComponent
+    SuggestionDetailsComponent,
+    SuggestionFormComponent
   ],
   imports: [
     CommonModule,
