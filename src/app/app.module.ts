d19002544ba47352c181349f0bb554a504879b2d
Bridgett Zaouga <BZaouga@se.linedata.com>
2026-05-10 19:35:35 +0100

    feat: ajout du composant listSuggestion dans core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ListSuggestionComponent
   ],
   imports: [
     BrowserModule,
